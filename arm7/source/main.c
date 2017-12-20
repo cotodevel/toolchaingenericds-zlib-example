f42d7498f9ce0f5777f03413305b88bd1a3461e8
Coto <programmer.coto.1988@gmail.com>
2017-12-20 00:59:44 -0400

    update example

--- arm7/source/main.c
+++ arm7/source/main.c
@@ -26,17 +26,16 @@ USA
 #include "wifi_arm7.h"
 #include "usrsettingsTGDS.h"
 #include "timerTGDS.h"
+#include "CPUARM.h"
 
 
 //---------------------------------------------------------------------------------
 int main(int _argc, sint8 **_argv) {
 //---------------------------------------------------------------------------------
-	
 	IRQInit();
-	
-	// Block execution until we get control of vram D
+	// Block execution until we get control of vram D + use VRAM stacks: allows to use 64K WRAM fully, malloc in VRAM and no slowdown
 	while (!(*((vuint8*)0x04000240) & 0x2));
-	
+	useARM7VRAMStacks();
 	installWifiFIFO();
 	
     while (1) {
