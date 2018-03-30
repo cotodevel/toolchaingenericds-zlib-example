9b24b7d36ce09046006ec0fd82dde83f6ee51eeb
cotodevel <programmer.coto.1988@gmail.com>
2018-03-30 03:14:15 -0300

    update arm7 code

--- arm7/source/main.c
+++ arm7/source/main.c
@@ -33,8 +33,6 @@ USA
 int main(int _argc, sint8 **_argv) {
 //---------------------------------------------------------------------------------
 	IRQInit();
-	while (!(*((vuint8*)0x04000240) & 0x2));
-	useARM7VRAMStacks();	//change ARM7 stacks to VRAM
 	installWifiFIFO();		//use DSWIFI
 	
     while (1) {
---
+++ arm9/build/dswnifi.d
@@ -0,0 +1,255 @@
+build/dswnifi.o: ../common/dswnifi.cpp ../common/specific_shared.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dsregs.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/typedefsTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/stdint.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/_default_types.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/features.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/stdbool.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/spitscTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dsregs_asm.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/global_settings.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/ipcfifoTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dsregs.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dsregs_asm.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/usrsettingsTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/time.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/_ansi.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/newlib.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/config.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/ieeefp.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/reent.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/_ansi.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/stddef.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/_types.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/_types.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/lock.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/time.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/types.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/types.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/memoryHandleTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/reent.h \
+ ../common/dswnifi.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/memoryHandleTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/wifi_shared.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/clockTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswnifi_lib.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/ipcfifoTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/wifi_shared.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/clockTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/biosTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/stdio.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/stdarg.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/stdio.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/string.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/cdefs.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/string.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/InterruptsARMCores_h.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/malloc.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/linkerTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/exceptionTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/ctype.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/stdlib.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/stdlib.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/alloca.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/select.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/stat.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/unistd.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/unistd.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/errno.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/errno.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/fcntl.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/fcntl.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/_default_fcntl.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/consoleTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswnifi_lib.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/netdb.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/wifi_arm9.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/sgIP_Hub.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/sgIP_Config.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/time.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/sgIP_memblock.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswifi9.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswifi_version.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/utilsTGDS.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/netdb.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/dirent.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/dirent.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/limits.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/limits.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/socket.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/in.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/socket.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/assert.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/wifi_arm9.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswifi9.h \
+ c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/utilsTGDS.h
+
+../common/specific_shared.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dsregs.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/typedefsTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/stdint.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/_default_types.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/features.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/stdbool.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/spitscTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dsregs_asm.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/global_settings.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/ipcfifoTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dsregs.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dsregs_asm.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/usrsettingsTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/time.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/_ansi.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/newlib.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/config.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/ieeefp.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/reent.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/_ansi.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/stddef.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/_types.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/_types.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/lock.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/time.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/types.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/types.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/memoryHandleTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/reent.h:
+
+../common/dswnifi.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/memoryHandleTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/wifi_shared.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/clockTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswnifi_lib.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/ipcfifoTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/wifi_shared.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/clockTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/biosTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/stdio.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/stdarg.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/stdio.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/string.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/cdefs.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/string.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/InterruptsARMCores_h.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/malloc.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/linkerTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/exceptionTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/ctype.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/stdlib.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/machine/stdlib.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/alloca.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/select.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/stat.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/unistd.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/unistd.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/errno.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/errno.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/fcntl.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/fcntl.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/_default_fcntl.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/consoleTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswnifi_lib.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/netdb.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/wifi_arm9.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/sgIP_Hub.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/sgIP_Config.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/sys/time.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/sgIP_memblock.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswifi9.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswifi_version.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/utilsTGDS.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/netdb.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/dirent.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/sys/dirent.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/limits.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/newlib-nds-headers/limits.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/socket.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/in.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/socket.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/include/assert.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/wifi_arm9.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/dswifi9.h:
+
+c:/toolchain_generic/6.2_2016q4/arm-none-eabi/lib/newlib-nano-2.1-nds/headers9/utilsTGDS.h:
