6390bbf7f23493b6bf0f9959fe7e39ba85557c3f
Coto <programmer.coto.1988@gmail.com>
2020-03-22 12:25:57 -0300

    Update Binary.
    TGDS Changes:
    	- update ARM7 printf

--- arm7/source/main.c
+++ arm7/source/main.c
@@ -32,7 +32,7 @@ int main(int _argc, sint8 **_argv) {
 	while (!(*((vuint8*)0x04000240) & 0x2));
 	ARM7DLDIInit();
 	
-	writePrintfBuffer7("TGDS ARM7.bin Boot OK!");
+	writeDebugBuffer7("TGDS ARM7.bin Boot OK!");
 	/*			TGDS 1.5 Standard ARM7 Init code end	*/
 	
     while (1) {
